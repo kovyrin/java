d286521dcd993eca7278b478f36a10be8b8402a2
Oleksiy Kovyrin <alexey@kovyrin.net>
2013-02-25 23:35:57 -0500

    Doh, invalid attr name

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,5 +1,5 @@
 # This recipe assumes you have jdk package somewhere in your apt/yum repository and installs it from there
 package "jdk" do
   action :install
-  version node.jdk_package_version
+  version node.java.jdk_package_version
 end
